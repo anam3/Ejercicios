f62481f29328eb4a7a59850ffb2c0ced432dba7a
anam3 <anam37999@gmail.com>
2016-11-22 09:50:12 -0600

    Add files via upload

---
+++ ConcursoTalentos-03/src/main/java/concursantes/EjecucionException.java
@@ -0,0 +1,12 @@
+package concursantes;
+
+public class EjecucionException extends RuntimeException {
+	public EjecucionException() {
+
+	}
+
+	public EjecucionException(String msg) {
+		super(msg);
+	}
+
+}
